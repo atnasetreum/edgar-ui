682173659922b78b982ffd144102971004656d88
Eduardo García <atnasetreum@gmail.com>
2023-02-02 13:54:25 -0600

    init edgar project

--- layouts/MainLayout/listItems.tsx
+++ layouts/MainLayout/listItems.tsx
@@ -5,14 +5,8 @@ import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useRouter } from "next/router";
-import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
-import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { AuthContext } from "../../contexts/auth";
 import { userTypes } from "../../constants";
-import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
-import LocationCityIcon from "@mui/icons-material/LocationCity";
-import AssessmentIcon from "@mui/icons-material/Assessment";
 
 export const MainListItems = () => {
   const router = useRouter();
@@ -31,72 +25,6 @@ export const MainListItems = () => {
         </ListItemIcon>
         <ListItemText primary="Inicio" />
       </ListItemButton>
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/sellers")}
-          selected={router.pathname === "/sellers"}
-        >
-          <ListItemIcon>
-            <ContactPhoneIcon />
-          </ListItemIcon>
-          <ListItemText primary="Sellers" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/rutas")}
-          selected={router.pathname === "/rutas"}
-        >
-          <ListItemIcon>
-            <LocationOnIcon />
-          </ListItemIcon>
-          <ListItemText primary="Rutas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/camionetas")}
-          selected={router.pathname === "/camionetas"}
-        >
-          <ListItemIcon>
-            <DirectionsCarIcon />
-          </ListItemIcon>
-          <ListItemText primary="Camionetas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/logisticas")}
-          selected={router.pathname === "/logisticas"}
-        >
-          <ListItemIcon>
-            <LocationCityIcon />
-          </ListItemIcon>
-          <ListItemText primary="Logisticas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/reportes")}
-          selected={router.pathname === "/reportes"}
-        >
-          <ListItemIcon>
-            <AssessmentIcon />
-          </ListItemIcon>
-          <ListItemText primary="Reportes" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/users")}
-          selected={router.pathname === "/users"}
-        >
-          <ListItemIcon>
-            <PeopleAltIcon />
-          </ListItemIcon>
-          <ListItemText primary="Users" />
-        </ListItemButton>
-      )}
     </React.Fragment>
   );
 };
